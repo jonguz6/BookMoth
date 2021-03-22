1feeb29da9cef81e43703067ae698e741aeac5ef
Jonatan <jonguz6@wp.pl>
2021-03-22 18:46:44 +0100

    Added CRUD views for Book model with basic templates

--- library/urls.py
+++ library/urls.py
@@ -2,5 +2,22 @@ from django.urls import path, include
 
 from library import views
 
+app_name = 'library'
 urlpatterns = [
+    path('', views.index, name="index"),
+    path(
+        'book-create/', views.BookCreateView.as_view(), name="book-create"
+    ),
+    path(
+        'book-list/', views.BookListView.as_view(), name="book-list"
+    ),
+    path(
+        'book-detail/<pk>', views.BookDetailView.as_view(), name="book-detail"
+    ),
+    path(
+        'book-update/<pk>', views.BookUpdateView.as_view(), name="book-update"
+    ),
+    path(
+        'book-delete/<pk>', views.BookDeleteView.as_view(), name="book-delete"
+    ),
 ]
--- library/views.py
+++ library/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+import django.views.generic as views
+from django.urls import reverse_lazy
 
-# Create your views here.
+from library import models
+
+
+class BookCreateView(views.CreateView):
+    model = models.Book
+    template_name = "book/book_create_template.html"
+    fields = "__all__"
+    success_url = reverse_lazy("library:book-list")
+
+
+class BookListView(views.ListView):
+    model = models.Book
+    template_name = "book/book_list_template.html"
+
+
+class BookDetailView(views.DetailView):
+    model = models.Book
+    template_name = "book/book_detail_template.html"
+
+
+class BookUpdateView(views.UpdateView):
+    model = models.Book
+    template_name = "book/book_update_template.html"
+    fields = "__all__"
+    success_url = reverse_lazy("library:book-list")
+
+
+class BookDeleteView(views.DeleteView):
+    model = models.Book
+    template_name = "book/book_delete_template.html"
+    success_url = reverse_lazy("library:book-list")
+
+
+def index(request):
+    return render(
+        request, template_name="book_base.html"
+    )
