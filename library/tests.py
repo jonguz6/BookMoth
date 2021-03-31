55b2fbba1d16d86bb611e600f43c935cf1912aaf
Jonatan <jonguz6@wp.pl>
2021-03-31 15:14:21 +0200

    test(LP): create test to past date field

--- library/tests.py
+++ library/tests.py
@@ -1,3 +1,21 @@
+from datetime import datetime, timedelta
+
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-# Create your tests here.
+from library.forms import PastDateField, UTC
+
+
+class PastDateFieldTest(TestCase):
+
+    def setUp(self):
+        self.past_date_field = PastDateField()
+
+    def test_validates_past_date(self):
+        past_date = datetime.today().replace(tzinfo=UTC) - timedelta(days=2)
+        self.past_date_field.validate(value=past_date)
+
+    def test_doesnt_validate_future_date(self):
+        future_date = datetime.today().replace(tzinfo=UTC) + timedelta(days=2)
+        with self.assertRaises(ValidationError):
+            self.past_date_field.validate(future_date)
