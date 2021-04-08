8f943889bae315f02c4d4966a64c664b23fb51f1
Jonatan <jonguz6@wp.pl>
2021-04-08 20:17:17 +0200

    feature(LP): CRUD for book instance

--- library/views.py
+++ library/views.py
@@ -35,6 +35,45 @@ class BookDeleteView(views.DeleteView):
     success_url = reverse_lazy("library:book-list")
 
 
+class BookInstanceCreateView(views.CreateView):
+    model = models.BookInstance
+    template_name = "book_instance/book_instance_create_template.html"
+    form_class = forms.BookInstanceForm
+    success_url = reverse_lazy("library:book-instance-list")
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        book = self.kwargs.get('book_id')
+        print(book)
+        if book is None:
+            return kwargs
+        kwargs['initial'] = {'book': models.BookInstance.objects.get(id=book)}
+        return kwargs
+
+
+class BookInstanceListView(views.ListView):
+    model = models.BookInstance
+    template_name = "book_instance/book_instance_list_template.html"
+
+
+class BookInstanceDetailView(views.DetailView):
+    model = models.BookInstance
+    template_name = "book_instance/book_instance_detail_template.html"
+
+
+class BookInstanceUpdateView(views.UpdateView):
+    model = models.BookInstance
+    template_name = "book_instance/book_instance_update_template.html"
+    form_class = forms.BookInstanceForm
+    success_url = reverse_lazy("library:book-instance-list")
+
+
+class BookInstanceDeleteView(views.DeleteView):
+    model = models.BookInstance
+    template_name = "book_instance/book_instance_delete_template.html"
+    success_url = reverse_lazy("library:book-instance-list")
+    
+    
 def index(request):
     return render(
         request, template_name="book/book_index.html"
