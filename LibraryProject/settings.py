5644320a92ff5b65b9443316ba0e8b3b64bd0cd2
j <jonguz6@wp.pl>
2021-05-13 15:36:49 +0200

    feature dropbox_storage

--- LibraryProject/settings.py
+++ LibraryProject/settings.py
@@ -131,9 +131,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
+DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+DROPBOX_OAUTH2_TOKEN = get_env_value('DROPBOX_OAUTH2_TOKEN')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
